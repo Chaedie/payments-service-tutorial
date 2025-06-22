4feb2c945b5818448c56dca5a01cb124f41a06d8
chaedongim <chaedong.im@cj.net>
2025-06-22 23:17:06 +0900

    initial commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+```shell
+docker pull mysql:8.2.0
+docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=1234 -d -p 3306:3306 mysql:8.2.0
+
+
+```
---
+++ src/test/kotlin/com/chaedie/paymentsservice/PaymentsServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chaedie.paymentsservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentsServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
